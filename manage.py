90707ee033640444ba1990a8cb61656272d0162e
singerlower <szh18266184828@163.com>
2017-08-04 19:23:19 +0800

    收到尽快付款

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
 
 
-深度方式是否  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polles.settings")
+深度方式是否  os.environ.setdefault("DJANGO_S啊外非3企业5ETTINGS_MODULE", "polles.settings")
 
     from django.core.management import execute_from_command_line
 
