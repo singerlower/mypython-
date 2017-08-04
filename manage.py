067381bf138499bace02a23c9563b98a1d9d0c0f
singerlower <szh18266184828@163.com>
2017-08-04 19:14:15 +0800

    水岸东方
    Signed-off-by: singerlower <szh18266184828@163.com>

--- manage.py
+++ manage.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polles.settings")
+  
+
+
+
+
+
+深度方式是否  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polles.settings")
 
     from django.core.management import execute_from_command_line
 
